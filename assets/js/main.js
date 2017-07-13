f469a8551d615654c5ee7fcd9dd33e6e7e40e415
“Erika <foglzerika@gmail.com>
2017-07-13 16:15:08 -0500

    Creando las tarjetas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,10 @@
+
+var arregloPlanetas = [];
+
+
 function getJSON(url){
 	return new Promise (function(resolve, error){
+		
 		var ajax = new XMLHttpRequest();
 		ajax.open("GET", url);
 		ajax.send();
@@ -12,6 +17,65 @@ function getJSON(url){
 		}
 	})
 }
+
 getJSON ("data/earth-like-results.json")
-	.then(function(mensaje){return getJSON(mensaje.results[0])})
-	.then(function(resultado) {console.log(resultado)})
+	.then(function(arreglo){
+		arreglo.results.forEach(function(elemento){
+			// console.log(elemento);
+			arregloPlanetas.push( getJSON(elemento))			
+			})
+		arregloPlanetas.forEach(function(elemento){
+			// console.log(elemento);
+			elemento.then(function(planeta){
+				imprimir(planeta);
+				console.log(planeta);
+			})
+		})
+
+		})
+var imprimir = function(elemento){
+	var nombre = elemento.pl_name;
+	var descubrimiento = elemento.pl_disc;
+	var telescopio = elemento.pl_telescope;
+	creaTarjeta(nombre,descubrimiento,telescopio);
+
+
+}
+var creaTarjeta = function(nombre,descubrimiento,telescopio){
+	 var contenedorPlanetas = document.getElementById("contenedor");
+ 	
+	  var row = document.createElement("div");
+	  var col_s12 = document.createElement("div");
+	  var card=document.createElement("div");
+	  var card_image= document.createElement("div");
+	  var card__image_imagen = document.createElement("img");
+	  var card__image_title = document.createElement("span");
+	  var card_content= document.createElement("div");
+	  var card_content_parrafo= document.createElement("p");
+
+	  row.setAttribute("class","row");
+	  col_s12.setAttribute("class","col s12 m6");
+	  card.setAttribute("class","card");
+	  card_image.setAttribute("class","card-image");
+	  card__image_imagen.setAttribute("src","static/img/planeta1.jpg");
+	  card__image_title.setAttribute("class","card-title");
+	  card_content.setAttribute("class","card-content");
+
+	  card__image_title.innerText = nombre ;
+	  card_content_parrafo.innerText = ("Descubierto en : "+descubrimiento+" con el "+telescopio);
+	  card_image.appendChild(card__image_imagen);
+	  card_image.appendChild(card__image_title);
+	  card_content.appendChild(card_content_parrafo)
+	  card.appendChild(card_content);
+	  card.appendChild(card_image);
+	  card.appendChild(card_content);
+	  col_s12.appendChild(card);
+	  row.appendChild(col_s12);
+	  contenedorPlanetas.appendChild(row);
+
+
+
+}
+		
+	// 	return getJSON(mensaje.results[0])})
+	// .then(function(resultado) {console.log(resultado.pl_name)})
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 
 app.use("/data", express.static(__dirname + '/data'));
+app.use("/static", express.static(__dirname + '/node_modules'));
 app.use("/static", express.static(__dirname+ '/assets'));
 
 app.get("/",function(req,res){
